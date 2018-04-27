594ce1ad9a5a23fde3c1f156db013ceb00ca1e10
zhouzhoufamily <512694189@qq.com>
2018-04-27 16:20:24 +0800

    1. add debug resource. 2. add test encode and decode.     add debugging command arguments:     Usage: encode [-e or -d] [in file] [out file]

--- speex_win/libspeex/testenc.c
+++ speex_win/libspeex/testenc.c
@@ -12,134 +12,203 @@ extern long long spx_mips;
 
 #define FRAME_SIZE 160
 #include <math.h>
+
+// wav头部结构体  
+struct wave_header {
+
+	char riff[4];
+	unsigned long fileLength;
+	char wavTag[4];
+	char fmt[4];
+	unsigned long size;
+	unsigned short formatTag;
+	unsigned short channel;
+	unsigned long sampleRate;
+	unsigned long bytePerSec;
+	unsigned short blockAlign;
+	unsigned short bitPerSample;
+	char data[4];
+	unsigned long dataSize;
+
+};
+
+
+void *createWaveHeader(int fileLength, short channel, int sampleRate, short bitPerSample)
+{
+
+	struct wave_header *header = malloc(sizeof(struct wave_header));
+
+	if (header == NULL) {
+		return  NULL;
+	}
+
+	// RIFF  
+	header->riff[0] = 'R';
+	header->riff[1] = 'I';
+	header->riff[2] = 'F';
+	header->riff[3] = 'F';
+
+	// file length  
+	header->fileLength = fileLength + (44 - 8);
+
+	// WAVE  
+	header->wavTag[0] = 'W';
+	header->wavTag[1] = 'A';
+	header->wavTag[2] = 'V';
+	header->wavTag[3] = 'E';
+
+	// fmt  
+	header->fmt[0] = 'f';
+	header->fmt[1] = 'm';
+	header->fmt[2] = 't';
+	header->fmt[3] = ' ';
+
+	header->size = 16;
+	header->formatTag = 1;
+	header->channel = channel;
+	header->sampleRate = sampleRate;
+	header->bitPerSample = bitPerSample;
+	header->blockAlign = (short)(header->channel * header->bitPerSample / 8);
+	header->bytePerSec = header->blockAlign * header->sampleRate;
+
+	// data  
+	header->data[0] = 'd';
+	header->data[1] = 'a';
+	header->data[2] = 't';
+	header->data[3] = 'a';
+
+	// data size  
+	header->dataSize = fileLength;
+
+	return header;
+}
+
+int encFun(char *inFile, char *outFile)
+{
+	FILE *fin, *fout, *fbits = NULL;//file streans
+	short in[FRAME_SIZE];
+	float input[FRAME_SIZE];
+	char cbits[200];
+	int nbBits;
+	void *state;//encode state
+	SpeexBits bits;
+	int i, tmp;
+	int bitCount = 0;//记录字节数
+
+	state = speex_encoder_init(speex_lib_get_mode(SPEEX_MODEID_NB));
+
+	tmp = 8;
+	speex_encoder_ctl(state, SPEEX_SET_QUALITY, &tmp);
+
+	fin = fopen(inFile, "rb");
+	fout = fopen(outFile, "wb+");
+
+	speex_bits_init(&bits);
+	while (!feof(fin))
+	{
+		fread(in, sizeof(short), FRAME_SIZE, fin);
+		if (feof(fin))
+			break;
+
+		for (size_t i = 0; i < FRAME_SIZE; i++)
+			input[i] = in[i];
+
+		speex_bits_reset(&bits);
+
+		speex_encode(state, input, &bits);
+		nbBits = speex_bits_write(&bits, cbits, 200);
+		bitCount += bits.nbBits;
+
+		speex_bits_reset(&bits);
+
+		fwrite(&nbBits, sizeof(int), 1, fout);
+		fwrite(cbits, 1, nbBits, fout);
+	}
+	fprintf(stderr, "Total encoded size: %d bits\n", bitCount);
+	speex_encoder_destroy(state);
+	speex_bits_destroy(&bits);
+
+	fclose(fin);
+	fclose(fout);
+
+	return 1;
+}
+
+int decFun(char *inFile, char *outFile)
+{
+	FILE *fin, *fout, *fbits = NULL;//file streans
+	short out[FRAME_SIZE];
+	float output[FRAME_SIZE];
+	char cbits[200];
+	int nbBits;
+	void *state;//decode state
+	SpeexBits bits;
+	int i, tmp;
+	int bitCount = 0;//记录字节数
+
+	state = speex_decoder_init(speex_lib_get_mode(SPEEX_MODEID_NB));
+
+	tmp = 1;
+	speex_decoder_ctl(state, SPEEX_SET_ENH, &tmp);
+
+	fin = fopen(inFile, "rb");
+	fout = fopen(outFile, "wb+");
+
+	//写入wav头部
+	char *header = createWaveHeader(0, 1, 8000, 16);
+	fwrite(header, 1, 44, fout);
+
+	speex_bits_init(&bits);
+	while (!feof(fin))
+	{
+		fread(&nbBits, sizeof(int), 1, fin);
+		if (feof(fin))
+			break;
+
+		fread(cbits, 1, nbBits, fin);
+
+		speex_bits_read_from(&bits, cbits, nbBits);
+
+		speex_decode(state, &bits, output);
+
+		for (size_t i = 0; i < FRAME_SIZE; i++)
+			out[i] = output[i];
+
+		bitCount += bits.nbBits;
+
+		fwrite(out, sizeof(short), FRAME_SIZE, fout);
+	}
+	fprintf(stderr, "Total encoded size: %d bits\n", bitCount);
+	speex_decoder_destroy(state);
+	speex_bits_destroy(&bits);
+
+	fclose(fin);
+	fclose(fout);
+
+	return 1;
+}
+
 int main(int argc, char **argv)
 {
-   char *inFile, *outFile, *bitsFile;
-   FILE *fin, *fout, *fbits=NULL;
-   short in_short[FRAME_SIZE];
-   short out_short[FRAME_SIZE];
-   int snr_frames = 0;
-   char cbits[200];
-   int nbBits;
-   int i;
-   void *st;
-   void *dec;
-   SpeexBits bits;
-   spx_int32_t tmp;
-   int bitCount=0;
-   spx_int32_t skip_group_delay;
-   SpeexCallback callback;
+	if (argc != 5 && argc != 4)
+	{
+		fprintf(stderr, "Usage: encode [-e or -d] [in file] [out file] [bits file]\nargc = %d", argc);
+		exit(1);
+	}
 
-   st = speex_encoder_init(speex_lib_get_mode(SPEEX_MODEID_NB));
-   dec = speex_decoder_init(speex_lib_get_mode(SPEEX_MODEID_NB));
-
-   /* BEGIN: You probably don't need the following in a real application */
-   callback.callback_id = SPEEX_INBAND_CHAR;
-   callback.func = speex_std_char_handler;
-   callback.data = stderr;
-   speex_decoder_ctl(dec, SPEEX_SET_HANDLER, &callback);
-
-   callback.callback_id = SPEEX_INBAND_MODE_REQUEST;
-   callback.func = speex_std_mode_request_handler;
-   callback.data = st;
-   speex_decoder_ctl(dec, SPEEX_SET_HANDLER, &callback);
-   /* END of unnecessary stuff */
-
-   tmp=1;
-   speex_decoder_ctl(dec, SPEEX_SET_ENH, &tmp);
-   tmp=0;
-   speex_encoder_ctl(st, SPEEX_SET_VBR, &tmp);
-   tmp=8;
-   speex_encoder_ctl(st, SPEEX_SET_QUALITY, &tmp);
-   tmp=1;
-   speex_encoder_ctl(st, SPEEX_SET_COMPLEXITY, &tmp);
-
-   /* Turn this off if you want to measure SNR (on by default) */
-   tmp=1;
-   speex_encoder_ctl(st, SPEEX_SET_HIGHPASS, &tmp);
-   speex_decoder_ctl(dec, SPEEX_SET_HIGHPASS, &tmp);
-
-   speex_encoder_ctl(st, SPEEX_GET_LOOKAHEAD, &skip_group_delay);
-   speex_decoder_ctl(dec, SPEEX_GET_LOOKAHEAD, &tmp);
-   skip_group_delay += tmp;
-
-   if (argc != 4 && argc != 3)
-   {
-      fprintf (stderr, "Usage: encode [in file] [out file] [bits file]\nargc = %d", argc);
-      exit(1);
-   }
-   inFile = argv[1];
-   fin = fopen(inFile, "rb");
-   outFile = argv[2];
-   fout = fopen(outFile, "wb+");
-   if (argc==4)
-   {
-      bitsFile = argv[3];
-      fbits = fopen(bitsFile, "wb");
-   }
-   speex_bits_init(&bits);
-   while (!feof(fin))
-   {
-      fread(in_short, sizeof(short), FRAME_SIZE, fin);
-      if (feof(fin))
-         break;
-      speex_bits_reset(&bits);
-
-      speex_encode_int(st, in_short, &bits);
-      nbBits = speex_bits_write(&bits, cbits, 200);
-      bitCount+=bits.nbBits;
-
-      if (argc==4)
-         fwrite(cbits, 1, nbBits, fbits);
-      speex_bits_rewind(&bits);
-
-      speex_decode_int(dec, &bits, out_short);
-      speex_bits_reset(&bits);
-
-      fwrite(&out_short[skip_group_delay], sizeof(short), FRAME_SIZE-skip_group_delay, fout);
-      skip_group_delay = 0;
-   }
-   fprintf (stderr, "Total encoded size: %d bits\n", bitCount);
-   speex_encoder_destroy(st);
-   speex_decoder_destroy(dec);
-   speex_bits_destroy(&bits);
-
-#ifndef DISABLE_FLOAT_API
-   {
-   float sigpow,errpow,snr, seg_snr=0;
-   sigpow = 0;
-   errpow = 0;
-
-   /* This code just computes SNR, so you don't need it either */
-   rewind(fin);
-   rewind(fout);
-
-   while ( FRAME_SIZE == fread(in_short, sizeof(short), FRAME_SIZE, fin)
-           &&
-           FRAME_SIZE ==  fread(out_short, sizeof(short), FRAME_SIZE,fout) )
-   {
-	float s=0, e=0;
-        for (i=0;i<FRAME_SIZE;++i) {
-            s += (float)in_short[i] * in_short[i];
-            e += ((float)in_short[i]-out_short[i]) * ((float)in_short[i]-out_short[i]);
-        }
-	seg_snr += 10*log10((s+160)/(e+160));
-	sigpow += s;
-	errpow += e;
-	snr_frames++;
-   }
-   snr = 10 * log10( sigpow / errpow );
-   seg_snr /= snr_frames;
-   fprintf(stderr,"SNR = %f\nsegmental SNR = %f\n",snr, seg_snr);
-
-#ifdef FIXED_DEBUG
-   printf ("Total: %f MIPS\n", (float)(1e-6*50*spx_mips/snr_frames));
-#endif
-   }
-#endif
-
-   fclose(fin);
-   fclose(fout);
+	if (strcmp("-e", argv[1]) == 0)
+	{
+		encFun(argv[2], argv[3]);
+	}
+	else if (strcmp("-d", argv[1]) == 0)
+	{
+		decFun(argv[2], argv[3]);
+	}
+	else
+	{
+		fprintf(stderr, "Usage: encode [-e or -d] [in file] [out file] [bits file]\nargc = %d", argc);
+		exit(1);
+	}
 
    return 0;
 }
